6d5b4d021e591708459283be01ea645feea3ed93
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-11-09 14:10:34 +0100

    Created component AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="title">
+        <h1>{{titolo}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                titolo: 'La mia prima app con Vite!'
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 150px;
+    }
+
+    h1 {
+        color: white;
+        font-weight: bold;
+    }
+
+</style>
